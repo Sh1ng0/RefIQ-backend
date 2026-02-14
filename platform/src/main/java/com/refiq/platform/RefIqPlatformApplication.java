56487306d6bbdd93a3c3d0ce5b52eef16122b8d6
Sh1ng0 <emildegas31416@gmail.com>
2026-02-14 20:16:43 +0100

    chore: removed leftover todo in the main class

--- platform/src/main/java/com/refiq/platform/RefIqPlatformApplication.java
+++ platform/src/main/java/com/refiq/platform/RefIqPlatformApplication.java
@@ -11,16 +11,6 @@ public class RefIqPlatformApplication {
   }
 
 
-  // TODO
-
-  // LOGGING estructurado para CSvNormalizer y el nuevo servicio
-  //         repasar que no haya magic strings en la codebase
-
-  // TESTING
-
-  // Revisar ultima versión de INgestionINtegrationTest, csvNormalizrTest, IngestionServiceTest y
-  // CalculationServiceIntegrationTest (All green)
-  // MOdificar el CalculationServiceTest debido a que ahora devolvemos JSON al front, no string (DONE)
 
   // Plantearse si normalizar todas las estrategias de test a Arrange Act Assert o Given When Then
 
